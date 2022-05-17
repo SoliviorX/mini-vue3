cfe594d22af4c4f180fe74b0d514ef23b7e1edaf
shideshan <307042873@qq.com>
2022-05-17 10:28:50 +0800

    feat: 实现track、trigger、runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,18 +2,62 @@
  * 创建effect时，首次会自动执行fn
  * 所以可以考虑使用class的方式实现effect
  */
-
 class ReactiveEffect {
   private _fn: any;
   constructor(fn) {
     this._fn = fn;
   }
   run() {
-    this._fn();
+    // 实例化的时候，将activeEffect赋值为当前实例
+    activeEffect = this;
+    // 将fn的返回值return出去
+    return this._fn();
   }
 }
 
+// 依赖收集；每个key对应一个独一无二的dep，在dep中收集activeEffect
+const targetMap = new Map();
+/**
+ * targetMap的结构及对应的类型和名称：
+ * targetMap<Map> {
+        target1<Map> : depsMap-{
+            key1<Set>: dep-[activeEffect],
+            key2<Set>: dep-[activeEffect],
+        },
+        target2<Map> : depsMap-{
+            key1<Set>: dep-[activeEffect]
+        },
+    }
+*/
+export function track(target, key) {
+  // target -> key -> dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+// 触发更新：获取到dep收集的所有effect，执行effect里面的回调
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  for (const effect of dep) {
+    effect.run();
+  }
+}
+
+let activeEffect;
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  // 返回runner，并将指针绑定为effect实例
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -7,6 +7,7 @@ import { effect } from '../effect';
  * 2. 用effect包裹一个回调函数
  * 3. 当响应式对象数据发生变化，相关effect的回调函数再次执行
  */
+
 describe('effect', () => {
   it('happy path', () => {
     const user = reactive({
@@ -23,5 +24,24 @@ describe('effect', () => {
     // 2. 当响应式对象数据发生变化，相关effect的回调函数再次执行
     user.age++;
     expect(nextAge).toBe(12);
+  });
+
+  /**
+   * 1. 调用effect(fn)后会返回一个function （runner）
+   * 2. 调用runner后，会执行effect的回调函数fn，并获得fn的返回值
+   */
+  it('should return runner when call effect', () => {
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return 'foo';
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    // 验证执行runner后，fn有没有再次执行
+    expect(foo).toBe(12);
+    // 验证执行runner有没有获取到fn的返回值
+    expect(r).toBe('foo');
   });
 });
