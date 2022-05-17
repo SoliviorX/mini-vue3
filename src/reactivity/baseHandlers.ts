ba93de1b55b316e09c3c44a60ebf287beab1e62f
shideshan <307042873@qq.com>
2022-05-17 18:13:41 +0800

    feat: 实现readonly

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,39 @@
+import { track, trigger } from './effect';
+
+function createGetter(isReadonly = false) {
+  return function (target, key) {
+    const res = Reflect.get(target, key);
+    // isReadonly属性不进行依赖收集
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function (target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // 触发依赖更新
+    trigger(target, key);
+    return res;
+  };
+}
+
+// 优化：将创建getter、setter缓存起来，而不是每次调用reactive/readonly都创建新的getter函数/setter函数
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key:${key} set 失败，因为 ${target} 是readonly。`);
+    return true;
+  },
+};
---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,21 @@
+import { readonly } from '../reactive';
+
+describe('readonly', () => {
+  it('happy path', () => {
+    // 不能改写，只能读取
+    const original = { foo: 1, bar: { baz: 2 } };
+    const wrapped = readonly(original);
+    expect(wrapped).not.toBe(original);
+    expect(wrapped.foo).toBe(1);
+  });
+
+  it('warn when call set', () => {
+    console.warn = jest.fn();
+    const user = readonly({
+      age: 10,
+    });
+    user.age = 11;
+    // 验证 console.warn 是否被调用
+    expect(console.warn).toBeCalled();
+  });
+});
