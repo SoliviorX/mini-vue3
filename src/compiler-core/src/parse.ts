b44c53079b5069f20c8dceac8759b3ffaa311d46
shideshan <307042873@qq.com>
2022-05-27 22:00:26 +0800

    feat: 实现解析简单标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast';
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
 
@@ -9,11 +14,45 @@ export function baseParse(content: string) {
 function parseChildren(context) {
   const nodes: any[] = [];
   let node;
-  if (context.source.startsWith('{{')) {
+  const s = context.source;
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
   return nodes;
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+
+  // TODO 解析元素内容
+
+  parseTag(context, TagType.End);
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  // 1. 解析tag
+  // 如果处理开始标签<div>，其处理结果是 [ '<div', 'div', index: 0, input: '<div></div>', groups: undefined ]
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source); // 该正则可以处理开始标签，也能处理结束标签
+  const tag = match[1];
+
+  // TODO 解析标签属性
+
+  // 2. template 删除处理完成的代码
+  advanceBy(context, match[0].length); // 删除标签的 '<'、标签名及属性（如果是开始标签的话）
+  advanceBy(context, 1); // 删除标签的右尖括号 '>'
+
+  // 如果处理的是结束标签，不需要返回值
+  if (type === TagType.End) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
